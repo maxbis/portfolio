1e5efd96417ea09f1efa7fcf524bef56b6882134
Max Bisschop <max@ffkijken.com>
2025-02-06 21:20:59 +0100

    Check before rfac model

--- app/views/transaction/edit.php
+++ app/views/transaction/edit.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- <?php
-echo "<pre>";
-print_r($exchanges);
-echo "</pre><br>";
-?> -->
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
